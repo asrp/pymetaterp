a3802a524691c4a2ef0daeee703612ba05cd0bc0
asrp <asrp@email.com>
2018-02-01 08:22:00 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(name='pymetaterp',
+      version='1.0',
+      description='A python parser that builds python ASTs in 502 lines of python without using modules',
+      url='https://github.com/asrp/pymetaterp',
+      author='asrp',
+      author_email='asrp@email.com',
+      packages=['pymetaterp'],
+      keywords='parser peg python minimal')
